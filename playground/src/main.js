179d15fc6e4cd01e18aa2344afb6b88d774a4ef4
manvfx <manvfx@gmail.com>
2023-08-29 15:28:04 +0330

    update playground

--- playground/src/main.js
+++ playground/src/main.js
@@ -5,5 +5,5 @@ const app = createApp(App);
 app.component('super-graph-view', defineAsyncComponent(() => import('../../mod')));
 
 
-import App from './App.vue';
+import App from './app.vue';
 app.mount('#app');
